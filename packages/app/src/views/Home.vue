1434a6daf85e0b8945fda3d90c9518fde43374f8
Stepan Sotnikov <sotnikovse1207@gmail.com>
2020-12-18 02:27:03 +0900

    build: app dependent ui
    build: added lerna command

--- packages/app/src/views/Home.vue
+++ packages/app/src/views/Home.vue
@@ -2,5 +2,17 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <h1 class="text-red-500">Welcome to Your Vue.js App</h1>
+    <div class="p-6">
+      <Btn>Button</Btn>
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+import { Btn } from 'ui'
+
+export default {
+  name: 'Home',
+  components: { Btn }
+}
+</script>
---
+++ packages/app/vue.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // https://cli.vuejs.org/guide/troubleshooting.html#symbolic-links-in-node-modules
+  chainWebpack: config => config.resolve.symlinks(false)
+}
